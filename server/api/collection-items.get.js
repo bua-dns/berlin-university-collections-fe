740640143391d962074ebf20560e3dacdbf903fe
culturetogo <m.mueller@culture-to-go.de>
2025-10-27 16:39:57 +0100

    server endpoint for mstub-items implemented

---
+++ server/api/collection-items.get.js
@@ -0,0 +1,17 @@
+export default defineEventHandler(async (event) => {
+  try {
+    const data = await $fetch('https://mstub-db.bua-dns.de/items/minerals', {
+      query: {
+        fields: '*.*',
+        limit: -1,
+      }
+    });
+
+    return data;
+  } catch (error) {
+    throw createError({
+      statusCode: error.statusCode || 500,
+      statusMessage: error.message || 'Failed to fetch collection items',
+    });
+  }
+});
