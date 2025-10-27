6c2f58b70964bc37452051af8808c2a109330a6c
culturetogo <m.mueller@culture-to-go.de>
2025-10-27 16:34:20 +0100

    scaffolded page for collective items display

---
+++ pages/items/[item].vue
@@ -0,0 +1,27 @@
+<script setup>
+const route = useRoute();
+const { locale } = useI18n();
+const theme = useState('theme');
+const w = theme.value.data.wording[locale.value];
+
+const slug = route.params['item']
+</script>
+<template>
+  <div class="page university-collection-item">
+
+    <Head>
+      <Title></Title>
+    </Head>
+    <h2 class="mb-4 text-center">{{ slug }}</h2>
+
+</div>
+  </template>
+<style lang="scss">
+.page.university-collection-item {
+  .backlink {
+    text-align: center;
+    margin: 1.5rem auto;
+  }
+  
+}
+</style>
